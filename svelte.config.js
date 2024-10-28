c4bd1d0d7b2f5fc7f4055070e20c37951fe11078
Ben Sievert <bsievert@precisionplanting.com>
2024-10-28 16:51:58 -0500

    WIP

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,8 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
-	
+
+
 	vitePlugin: {
 		inspector: true,
 	},
