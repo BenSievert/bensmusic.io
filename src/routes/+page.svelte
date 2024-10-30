62871cd6d430aedda3ae5f5bb6727c03a6d4e365
Ben Sievert <sievertbenjamin@gmail.com>
2024-10-30 08:28:51 -0500

    margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div class="min-h-screen bg-background-light px-6 py-4">
 	<Navigation />
 
-	<h1 class="mb-4 font-display text-3xl text-primary-dark md:text-5xl">Ben's Music Lessons</h1>
+	<h1 class="mb-1 font-display text-3xl text-primary-dark md:text-5xl">Ben's Music Lessons</h1>
 
 	<p class="mb-8 max-w-lg text-xl text-secondary-dark md:text-2xl">Holistic Music Education</p>
 </div>
