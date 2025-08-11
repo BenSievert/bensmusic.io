1ff805f92142394b86c9dd7034dd5cdfa449d2a2
Ben Sievert <sievertbenjamin@gmail.com>
2025-08-11 10:53:34 -0500

    prettier

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -15,10 +15,12 @@
 
 <nav class="flex items-center justify-end px-4 pt-4">
 	<button
-		class="bg-primary cursor-pointer rounded-lg px-1 text-lg text-gray-900 md:hidden"
+		class="bg-primary cursor-pointer rounded-lg text-2xl px-[3px] py-[2px] text-gray-700 md:hidden"
 		on:click={toggleMenu}
 	>
-		&#9776;
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+		</svg>
 	</button>
 	<div class="hidden space-x-4 md:flex">
 		{#each links as link}
@@ -39,10 +41,12 @@
 			<a
 				on:click={toggleMenu}
 				href={link.href}
-				class="text-accent-dark hover:bg-highlight-mintGreen p-2 hover:text-rose-700"
+				class="hover:bg-highlight-mintGreen p-2 text-rose-700"
 			>
 				{link.name}
 			</a>
+			<hr class="text-blue-200">
+
 		{/each}
 	</div>
 </nav>
