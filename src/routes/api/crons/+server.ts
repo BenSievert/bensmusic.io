13f8f606032a7c4f26c214a5f1fa0b822527952e
Ben Sievert <sievertbenjamin@gmail.com>
2025-08-20 07:34:56 -0500

    set active

--- src/routes/api/crons/+server.ts
+++ src/routes/api/crons/+server.ts
@@ -11,8 +11,9 @@ export const GET = async ({request}) => {
     return new Response('Unauthorized', {status: 401});
   }
   const [guitarist] = await sql`SELECT * FROM guitarists WHERE active = FALSE ORDER BY id LIMIT 1`
-  console.log(guitarist)
-  return new Response('Hello Cron!');
+  await sql`UPDATE guitarists SET active = TRUE WHERE id = ${guitarist.id}`
+
+  return new Response(`New Guitarist ${guitarist.name}`);
 
 }
 
