df3c3d3be72af3f23d7978688aec6760efcc3456
Ben Sievert <sievertbenjamin@gmail.com>
2025-08-20 06:40:07 -0500

    test

--- src/routes/api/crons/+server.ts
+++ src/routes/api/crons/+server.ts
@@ -1,52 +1,23 @@
-/*
-import {PinpointClient, SendMessagesCommand } from "@aws-sdk/client-pinpoint";
-import { fromEnv } from "@aws-sdk/credential-providers";
-import type { RequestHandler } from './$types';
-import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } from "$env/static/private";
-process.env.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID;
-process.env.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY;
-process.env.AWS_REGION = AWS_REGION;
 
-export const GET: RequestHandler = async (req) => {
-  const client = new PinpointClient({
-    credentials: fromEnv()
-  });
-  console.log(client)
+import sql from '../../../postgres.server';
+import {
+  CRON_SECRET
+} from '$env/static/private';
 
-  const message =
-    "This message was sent through Amazon Pinpoint " +
-    "using the AWS SDK for JavaScript in Node.js. Reply STOP to " +
-    "opt out.";
 
-  const command = new SendMessagesCommand({
-    ApplicationId: `3047ba65a57646d890877439241f04f4`,
-    MessageRequest: {
-      Addresses: {
-        [`+12173791821`]: {
-          ChannelType: "SMS",
-        },
-      },
-      MessageConfiguration: {
-        SMSMessage: {
-          Body: message,
-          MessageType: `TRANSACTIONAL`,
-        },
-      },
-    },
-  });
 
-  try {
-    const data = await client.send(command);
-    console.log(
-      `Message sent! ${JSON.stringify(data)}`,
-    );
-  } catch (err) {
-    console.log(err);
+export const GET = async ({req, res}) => {
+  if (req.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
+    return res.status(401).end('Unauthorized');
   }
-
+  const [guitarist] = await sql`SELECT * FROM guitarists WHERE active = FALSE ORDER BY id LIMIT 1`
+  console.log(guitarist)
   return new Response('Hello Cron!');
 
 }
 
 
- */
+
+
+
+
