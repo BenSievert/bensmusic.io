7b8d858e9cfa6e4ad1f17e6fc275bcbc36aa0539
Benjamin Sievert <45344686+BenSievert@users.noreply.github.com>
2025-08-24 16:06:50 -0500

    Update +page.server.ts

--- src/routes/guitarist-of-the-week/+page.server.ts
+++ src/routes/guitarist-of-the-week/+page.server.ts
@@ -5,7 +5,7 @@ export const load: PageServerLoad = async ({ params }) => {
 	const guitarist = (
 		await sql`SELECT *, TO_CHAR(start,'Month DD, YYYY') date
 			  FROM guitarists
-			  where start <= NOW()
+			  where start + INTERVAL '6 HOURS' <= NOW()
 			  ORDER BY start DESC LIMIT 1`
 	)[0];
 
