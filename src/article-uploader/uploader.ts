c45e682e303b0efcb06603b543e10526252c2f5b
Ben Sievert <sievertbenjamin@gmail.com>
2025-09-30 09:17:48 -0500

    stuff

--- src/article-uploader/uploader.ts
+++ src/article-uploader/uploader.ts
@@ -1,5 +1,21 @@
 import { config } from 'dotenv';
+import  fs from 'fs';
 import { readFile } from 'fs/promises';
+
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+const askQuestion = async (question: string) => {
+	return new Promise((resolve) => {
+		rl.question(question, (answer) => {
+			resolve(answer);
+		});
+	});
+};
 
 config({ path: '../../.env.development.local' });
 
@@ -8,12 +24,30 @@ import { neon } from '@neondatabase/serverless';
 const connectionString: string = process.env.DATABASE_URL as string;
 const sql = neon(connectionString);
 
-const [fileName, update] = process.argv.slice(2);
-const name = fileName.replaceAll(`_`, ` `);
-readFile(`./${fileName}.md`, 'utf8').then(async (content) => {
-	const resp = await (update
-		? sql`UPDATE articles SET content = ${content} WHERE name = ${name}`
-		: sql`INSERT INTO articles (name, content) VALUES (${name}, ${content})`);
+fs.readdir('./articles', async (_: NodeJS.ErrnoException | null, files: string[]) => {
+	files.forEach((file, i) => console.log(`${i + 1}) ${file.slice(0, -3)}`))
 
-	console.log(resp);
+	const input = await askQuestion(`\nEnter article number: `)
+	const chosenFile = files[input as number - 1]
+	const chosenName = chosenFile.slice(0, -3)
+
+	readFile(`./articles/${chosenFile}`, 'utf8').then(async (content: string) => {
+
+		const exists = (((await sql`SELECT COUNT(*) FROM articles WHERE name = ${chosenName}` as { count: number}[])[0])).count;
+		if (exists) {
+			console.log(`Updating Article`)
+			await sql`UPDATE articles SET content = ${content} WHERE name = ${chosenName}`
+		}
+		else {
+			console.log(`Creating Article`)
+			await sql`INSERT INTO articles (name, content) VALUES (${chosenName}, ${content})`;
+		}
+
+		process.exit(0);
+
+	});
+
 });
+
+
+
