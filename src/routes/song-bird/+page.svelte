74fc179476aeb7d55fc4294ec33f306a513d594d
Ben Sievert <sievertbenjamin@gmail.com>
2025-10-15 08:49:43 -0500

    mobile friendly

--- src/routes/song-bird/+page.svelte
+++ src/routes/song-bird/+page.svelte
@@ -6,17 +6,18 @@
 
 	let { data }: { data: PageData } = $props();
 	const { session, songs } = data;
-	const headers = [`Title`, `Created`, `Last Updated`, ``];
+	const headers = [[`Title`, `title`], [`Created`, `date_created`], [`Last Updated`, `last_updated`], [``, `id`]];
 </script>
 
 <SitePage title="Song Bird" subtitle="Write and Arrange Songs">
 	<Section theme="secondary">
 		{#if session}
 			<a href="/song-bird/new" class="text-primary-dark hover:text-primary cursor-pointer text-lg flex items-center"><span class="text-2xl mr-1.5">+</span> Start New Song</a>
-			<table class="table-auto w-full mt-2">
+			<div class="relative">
+			<table class="table-auto w-full mt-2 invisible md:visible absolute">
 				<thead class="text-primary-dark text-lg">
 					<tr>
-						{#each headers as header}
+						{#each headers as [header]}
 							<th class=" text-left p-1">{header}</th>
 						{/each}
 					</tr>
@@ -49,6 +50,38 @@
 					{/each}
 				</tbody>
 			</table>
+			<div class="visible md:invisible absolute grid grid-cols-1 content-center mt-2 w-full text-sm">
+				{#each songs as song}
+					<a class="shadow rounded-md py-3 flex justify-between px-4 items-center gap-8 mb-8 border border-gray-200" href="/song-bird/{song.id}">
+						<div class="w-full">
+						{#each headers.slice(0,-1) as [header, key]}
+							<div class="flex justify-between gap-2 mb-2 items-center">
+								<div class="text-primary-dark font-bold flex-1">{header}</div>
+								<div class="text-left flex-1">{song[key]}</div>
+							</div>
+						{/each}
+						</div>
+						<div class="text-primary-dark">
+							<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="mb-[2px] size-6"
+							>
+								<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+								/>
+							</svg>
+						</div>
+					</a>
+				{/each}
+
+			</div>
+			</div>
 		{:else}
 			You must be logged in to see this page.
 		{/if}
