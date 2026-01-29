c3008cea3a2b2cac703aa12d94f9fbd3e0f2a724
Ben Sievert <sievertbenjamin@gmail.com>
2026-01-29 10:47:42 -0600

    inner layout

--- src/+layout.server.ts
+++ src/+layout.server.ts
@@ -1,10 +1,8 @@
 import type { LayoutServerLoad } from './$types';
-import { getStudentByEmail } from '$lib/server/functions';
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.auth();
 	return {
 		session,
-		student: session && (await getStudentByEmail(session))
 	};
 };
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,15 @@
 import type { LayoutServerLoad } from '../$types';
 import { ROOT_EMAIL } from '$env/static/private';
-import { getSession } from '$lib/server/functions';
+import {getSession, getStudentByEmail} from '$lib/server/functions';
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await getSession(event.locals);
 	const isRoot = session?.user?.email == ROOT_EMAIL;
+
 	return {
 		session,
-		isRoot
+		isRoot,
+		student: session && (await getStudentByEmail(session))
+
 	};
 };
